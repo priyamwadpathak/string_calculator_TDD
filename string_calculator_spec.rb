0e0229cde982841e3d88533e6ffaf568185385cd
Priyamwad Pathak <pathak.priyamvad17@gmail.com>
2025-04-24 19:14:18 +0530

    Write first test case for empty string and let it fail

---
+++ string_calculator_spec.rb
@@ -0,0 +1,9 @@
+require_relative 'string_calculator'
+require 'rspec'
+
+RSpec.describe StringCalculator do
+	it 'returns 0 for an empty string' do
+		calculator = StringCalculator.new
+		expect(calculator.add('').to eq(0))
+	end
+end

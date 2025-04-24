41102c4efcb558c4ab80700a9d2c443d8df47e74
Priyamwad Pathak <pathak.priyamvad17@gmail.com>
2025-04-24 19:17:53 +0530

    Define add method for emtpy string to pass the first test case

---
+++ string_calculator.rb
@@ -0,0 +1,7 @@
+class StringCalculator
+	def add(numbers)
+		if numbers.strip.empty?
+			return 0
+		end
+	end
+end
--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -4,6 +4,6 @@ require 'rspec'
 RSpec.describe StringCalculator do
 	it 'returns 0 for an empty string' do
 		calculator = StringCalculator.new
-		expect(calculator.add('').to eq(0))
+		expect(calculator.add('')).to eq(0)
 	end
 end

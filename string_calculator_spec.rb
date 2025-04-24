bc37584a4bad55fbd3aa5b6f75e94cf54d4a12fd
Priyamwad Pathak <pathak.priyamvad17@gmail.com>
2025-04-24 19:21:59 +0530

    Add test case for a single digit and let it fail

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -2,8 +2,13 @@ require_relative 'string_calculator'
 require 'rspec'
 
 RSpec.describe StringCalculator do
+	let(:calculator) { StringCalculator.new }
+
 	it 'returns 0 for an empty string' do
-		calculator = StringCalculator.new
 		expect(calculator.add('')).to eq(0)
+	end
+
+	it 'returns the same number for single digit' do
+		expect(calculator.add('5')).to eq(5)
 	end
 end

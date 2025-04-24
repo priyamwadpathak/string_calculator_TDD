ba584ed6cb1558e86355f1dae8505bc7ef430231
Priyamwad Pathak <pathak.priyamvad17@gmail.com>
2025-04-24 19:54:36 +0530

    Refactor the code

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,20 +2,11 @@ class StringCalculator
 	def add(numbers)
 		numbers = numbers.strip
 		return 0 if numbers.empty?
-			
-		return numbers.to_i if numbers.length == 1
 
-		# delimiter = [',', '\n']
 		delimiter = /,|\n/
 
 		nums = numbers.split(delimiter).map(&:to_i)
 
 		nums.sum
 	end
-
-	private
-
-	# def sum_two_numbers(nums)
-	# 	nums.split(',').map(&:to_i).sum
-	# end
 end

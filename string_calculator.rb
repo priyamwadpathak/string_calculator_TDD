a8e56d327ccde4efe480e74d052dba23c114523d
Priyamwad Pathak <pathak.priyamvad17@gmail.com>
2025-04-24 19:58:46 +0530

    Update code to pass the custom delimeters case

--- string_calculator.rb
+++ string_calculator.rb
@@ -4,8 +4,14 @@ class StringCalculator
 		return 0 if numbers.empty?
 
 		delimiter = /,|\n/
+		
+		if numbers.start_with?('//')
+			delimiter_line, numbers = numbers.split("\n", 2)
+			custom_delimiter = delimiter_line[2..]
+			delimiter = Regexp.escape(custom_delimiter)
+		end
 
-		nums = numbers.split(delimiter).map(&:to_i)
+		nums = numbers.split(/#{delimiter}/).map(&:to_i)
 
 		nums.sum
 	end
